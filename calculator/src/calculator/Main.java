2c34053dd9ff463413d7f7702d7b9283f5008963
nihol <niholasmacdonald@gmail.com>
2022-11-03 00:01:01 +0000

    comments added describing plan

--- calculator/src/calculator/Main.java
+++ calculator/src/calculator/Main.java
@@ -4,9 +4,36 @@ import java.util.HashMap;
 
 public class Main {
 
+	HashMap<String, Integer> registers = new HashMap<String, Integer>();
+
 	public static void main(String[] args) {
-		HashMap<String, Integer> registers = new HashMap<String, Integer>();
+		// read input into a string
 		
+		// determine if string is correct format
+		
+		// determine what type of string we have
+		
+		// pass instruction to queue or stack depending on if it uses values or just registers
+		
+		// repeat while waiting on print instruction
+		
+		// take instruction from front of queue
+		
+		// identify if registers are new 
+		
+		// create any new registers
+		
+		// adjust register with value presented
+		
+		// repeat while we empty queue
+		
+		// take instruction from top of stack
+		
+		// adjust register value
+		
+		// repeat until stack is empty
+		
+		// print result to console
 		
 	}
 
