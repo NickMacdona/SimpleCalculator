0600ec15b3cfabd80b706346e98a5cb1f786ebb8
nihol <niholasmacdonald@gmail.com>
2022-11-03 02:32:40 +0000

    Code fleshed out,
    out of bounds errors line 76
    probably more on repeated code from 76
    probably other errors
    not sure .poll() works as understood

--- calculator/src/calculator/Main.java
+++ calculator/src/calculator/Main.java
@@ -7,41 +7,51 @@ import java.util.Scanner;
 
 public class Main {
 
-	HashMap<String, Integer> registers = new HashMap<String, Integer>();
+	static HashMap<String, Integer> registers = new HashMap<String, Integer>();
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		Deque<String> queue = new LinkedList<String>();
 		Deque<String> stack = new LinkedList<String>();
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter command");
+		String[] printwords = null;
 		
 		while (true) {
+			System.out.println("Enter command");
 			// read input into a string
 			String command = scanner.nextLine();
 			command.toLowerCase();
-			if(command=="print") {
-				scanner.close();
+			if(command.contains("print")) {
+				printwords = command.split("\\s+");
 				break;
 			}
 				
-			if (command == "quit") {
+			if (command.equals("quit")) {
 				scanner.close();
 				System.exit(0);
 			}
 			;
 			// determine if string is correct format
 			String[] commandword = command.split("\\s+");
+			System.out.println(commandword[0]);
+			System.out.println(commandword[1]);
+			System.out.println(commandword[2]);
 			if (commandword.length > 3) {
-				System.out.println("Command not recognised");				
+				System.out.println("Command not recognised (length)");
+				continue;
 			}
 			if (checkIfCommand(commandword[1]) == false) {
-				System.out.println("Command not recognised");
+				System.out.println("Command not recognised (unknown command)");
+				continue;
 			}
 			if (checkIfRegisterValid(commandword[0]) == false) {
-				System.out.println("Command not recognised");
+				System.out.println("Command not recognised (register invalid)");
+				continue;
 			}
-			
+			if (checkIfRegisterValid(commandword[2]) == false || checkIfNum(commandword[2]) == false) {
+				System.out.println("Command not recognised (third word is not a register or number)");
+				continue;
+			}
 			// determine what type of string we have
 			// pass instruction to queue or stack depending on if it uses values or just registers
 			if (checkIfNum(commandword[2])) {
@@ -50,24 +60,92 @@ public class Main {
 				stack.add(command);
 			} 
 		}
+		
+		
+//		System.out.println(queue.size());
+//		for (String String : queue) {
+//			System.out.println(queue.pollFirst());
+//		}
+		//System.out.println(stack);
+		
 
+		String currentqueue = "start";
 		// take instruction from front of queue
-		
-		// identify if registers are new 
-		
-		// create any new registers
-		
+		while (currentqueue != null) {
+			currentqueue = queue.poll();//TODO scared this will break
+			String [] instructionword = currentqueue.split("\\s+");
+
+			// identify if registers are new 
+			if (registers.containsKey(instructionword[0]) == false) {
+				
+				// create any new registers
+				registers.put(instructionword[0], 0);
+			}
 		// adjust register with value presented
+			adjustQueueRegister(instructionword[0], instructionword[1], instructionword[2]);
+						
 		
 		// repeat while we empty queue
-		
+		}
+		String currentstack = "start";
 		// take instruction from top of stack
+		while (currentstack != null) {
+			currentstack = queue.poll();//TODO scared this will break
+			String [] instructionword = currentstack.split("\\s+");
+
+			// identify if registers are new 
+			if (registers.containsKey(instructionword[0]) == false) {
+				// create any new registers
+				registers.put(instructionword[0], 0);
+			}
+			if (registers.containsKey(instructionword[2]) == false) {
+				// create any new registers
+				registers.put(instructionword[2], 0);
+			}
+		// adjust register with value presented
+			adjustStackRegister(instructionword[0], instructionword[1], instructionword[2]);
+						
+		// repeat while we empty stack
+		}
+
+		// get result
+		int result = registers.getOrDefault(printwords[1], null);
+		// print results to console
+		System.out.println(result);
 		
-		// adjust register value
+	}
+	public static void adjustQueueRegister(String register, String operation, String number) {
+		int value = Integer.parseInt(number);
+		int regvalue = registers.getOrDefault(register, 0);
+		switch(operation) {
+			case "add":
+				registers.put(register, (value+regvalue));
+				break;
+			case "subtract":
+				registers.put(register, (value-regvalue));
+				break;
+			case "multiply":
+				registers.put(register, (value*regvalue));
+				break;	
+		}
 		
-		// repeat until stack is empty
+	}
+	
+	public static void adjustStackRegister(String register, String operation, String register2) {
+		int regvalue = registers.getOrDefault(register, 0);
+		int regvalue2 = registers.getOrDefault(register2, 0);
+		switch(operation) {
+			case "add":
+				registers.put(register, (regvalue+regvalue2));
+				break;
+			case "subtract":
+				registers.put(register, (regvalue-regvalue2));
+				break;
+			case "multiply":
+				registers.put(register, (regvalue*regvalue2));
+				break;
+		}
 		
-		// print result to console
 	}
 	
 	public static boolean checkIfNum(String string) {
@@ -84,16 +162,13 @@ public class Main {
 	}
 	
 	public static boolean checkIfCommand(String string) {
-		if (string == "add") {
+		if (string.equals("add")) {
 			return true;
-		}
-		else if(string == "subtract") {
+		} else if(string.equals("subtract")) {
 			return true;			
-		}
-		else if(string == "multiply") {
+		} else if(string.equals("multiply")) {
 			return true;			
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
